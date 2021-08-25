78161c2023da38be321855db575cb104c01deacf
kramer84 <kristof.attila.private@gmail.com>
2021-08-25 16:42:34 +0200

    updated quadMesh analysis

--- quadMeshProcessing.pyx
+++ quadMeshProcessing.pyx
@@ -3,15 +3,18 @@ import cython
 from scipy.spatial import kdtree, Rectangle
 import numpy as np
 cimport numpy as np
+
 from cython.parallel import prange
 from libcpp.vector cimport vector
 from libcpp cimport bool
 from libc.math cimport sin, cos, acos, exp, sqrt, fabs, M_PI
+from libc.stdio cimport printf
 
 np.import_array()
 
 cdef class quadMeshProcessing :
 
+    cdef int verbose
     cdef double[:,:] verts_mv
     cdef double[:,:,:] quads_mv 
     cdef int[:,:] faces_mv
@@ -26,8 +29,10 @@ cdef class quadMeshProcessing :
     cdef readonly object vertices_label_map
     cdef readonly object faces_label_map
     cdef readonly object face_connectivity_matrix
+
     def __init__(self, verts, faces, 
-                    verts_label_map=None, faces_label_map=None):
+                    verts_label_map=None, faces_label_map=None, verbose=0):
+        self.verbose = verbose
         self.faces = faces.copy()
         self.vertices = verts.copy()
         self.vertices_label_map = verts_label_map.copy() if verts_label_map is not None \
@@ -42,8 +47,6 @@ cdef class quadMeshProcessing :
         self.face_connectivity_matrix = np.zeros((self.faces.shape[0], 
                                                   self.faces.shape[0]), dtype='uint8')
 
-        print('Faces.shape =',faces.shape,'Verts.shape =',verts.shape)
-        
         self.quads = self.getQuads()
         self.correct_winding_faces()
         self.computeNormals()
@@ -62,7 +65,36 @@ cdef class quadMeshProcessing :
         """
         pass
 
-    cdef computeNormals(self):
+
+    def getGlobalBounds(self):
+        """Returns the bounds of the mesh in its absolute coordinates
+        """
+        if self.normals_mv.shape[0] != self.faces_mv.shape[0] :
+            if self.quads_mv.shape[0] != self.faces_mv.shape[0] :
+                self.quads = self.getQuads()
+                self.computeNormals()
+            else : 
+                self.computeNormals()
+        x_min = np.min(self.quads_mv[:,:,0])
+        x_max = np.max(self.quads_mv[:,:,0])
+        y_min = np.min(self.quads_mv[:,:,1])
+        y_max = np.max(self.quads_mv[:,:,1])
+        z_min = np.min(self.quads_mv[:,:,2])
+        z_max = np.max(self.quads_mv[:,:,2])
+        bounds = np.asarray([[x_min ,x_max], [y_min ,y_max], [z_min ,z_max]])
+        return bounds
+
+    def getLocalBounds(self):
+        """Returns the bounds, but in the relative coordinate system. The minimal
+        values are all 0 
+        """
+        bounds = self.getGlobalBounds()
+        relative_bounds = np.subtract(bounds, np.expand_dims(bounds[:, 0], axis = 1))   
+        return relative_bounds
+
+    @cython.wraparound(False)
+    @cython.boundscheck(False)
+    cdef void computeNormals(self) nogil:
         """We have to correct the windings, so that each pair of consecutive nodes in the faces
         represent a edge (if the perimeter is abcd, we should no have acbd for example)
         """
@@ -71,12 +103,13 @@ cdef class quadMeshProcessing :
 
         n_quads = self.quads_mv.shape[0]
 
-        for i in range(n_quads):
+        for i in prange(n_quads):
             normal_vect.clear()
             normal_vect = quadNormal(self.quads_mv[i])
             for j in range(3):
                 self.normals_mv[i,j]=normal_vect[j]
-        print('    Normals computed')
+        if self.verbose > 0 :
+            printf('    Normals computed')
 
 
     cdef correct_winding_faces(self):
@@ -89,12 +122,13 @@ cdef class quadMeshProcessing :
         cdef double[:,:] quad_mv
         cdef bool isSame
         cdef int isCooplanar
+        cdef SymmetricMatrix quad_distance_matrix = SymmetricMatrix(4)
 
         n_quads = self.quads_mv.shape[0]
 
         for i in range(n_quads):
             quad_mv = self.quads_mv[i,:,:]
-            new_indexing = self.correct_winding_1face(quad_mv)
+            new_indexing = self.correct_winding_1face(quad_mv, quad_distance_matrix)
             isCooplanar = self.checkIfFaceCooplanar(quad_mv)
             #if isCooplanar == 0 :
             #    print('Is not cooplanar')
@@ -108,10 +142,14 @@ cdef class quadMeshProcessing :
         self.setMemoryViews(self.faces, self.vertices, 
                 self.vertices_label_map, self.faces_label_map, self.normals)
         self.quads = self.getQuads()
-        print('    winding corrected')
+        if self.verbose > 0 :
+            print('    winding corrected')
 
 
-    cdef int checkIfFaceCooplanar(self, double[:,:] quad, double thresh = 1e-10):
+    @cython.wraparound(False)
+    @cython.boundscheck(False)
+    @cython.cdivision(True)
+    cdef int checkIfFaceCooplanar(self, double[:,:] quad, double thresh = 1e-10) nogil:
         cdef double a,b,c,d, a1, b1, c1, a2, b2, c2, dist
         cdef bool isCooplanar
         a1 = quad[1,0] - quad[0,0]
@@ -132,12 +170,11 @@ cdef class quadMeshProcessing :
         else: 
             return 0
 
-    cdef Py_ssize_t[:] correct_winding_1face(self, double[:,:] quad_face):
+    cdef Py_ssize_t[:] correct_winding_1face(self, double[:,:] quad_face, SymmetricMatrix distance_matrix ):
         """Checks the winding for one face and returns the new
         order of the indices 
         """
         cdef :
-            SymmetricMatrix distance_matrix 
             Py_ssize_t[4] basic_indexing = [0,1,2,3]
             Py_ssize_t[4] new_indexing = [0,1,2,3]
             Py_ssize_t[8][2] edgeIdx = [[0,1], [1,2], [2,3], [3,0], [1,0], [2,1], [3,2], [0,3]]
@@ -151,7 +188,7 @@ cdef class quadMeshProcessing :
             vector[bool] diff 
             bool sameVal0, sameVal1
 
-        distance_matrix = intra_face_distance_matrix(quad_face)
+        intra_face_distance_matrix(quad_face, distance_matrix)
         # we keep the first node at the right index, and then we check the distance betwwen the 
         # different nodes. We know that the max distance represents a diagonal. 
         # (at least one diagonal is the max) if the quad element is CONVEX
@@ -193,7 +230,7 @@ cdef class quadMeshProcessing :
                         i_diag_idx = 1 
                 diff.clear()
             
-            print('We have to switch index',edge[i_diag_idx],'with',diagIdx[i_diag][i_diag_idx])
+            #print('We have to switch index',edge[i_diag_idx],'with',diagIdx[i_diag][i_diag_idx])
 
             new_indexing[edge[i_diag_idx]] = basic_indexing[diagIdx[i_diag][i_diag_idx]]
             new_indexing[diagIdx[i_diag][i_diag_idx]] = basic_indexing[edge[i_diag_idx]]
@@ -203,15 +240,15 @@ cdef class quadMeshProcessing :
         if inDiagIdx >= 0 :
             return basic_indexing
 
-        else :
-            raise Exception("Shit happened")
+        #else :
+        #    raise Exception("Shit happened")
         
     #########################################################################
 
     @cython.wraparound(False)
     @cython.boundscheck(False)
     cdef getQuads(self):
-        cdef size_t[:] fs = np.array(self.faces_mv.shape, dtype = np.uint)
+        cdef size_t[:] fs = np.asarray(self.faces_mv.shape, dtype = np.uint)
         cdef np.ndarray[np.float64_t, ndim=3] nquads = np.zeros(shape=(fs[0], fs[1], 3), dtype = "float64")
         cdef size_t i, j, p
         self.vquads.clear()
@@ -271,7 +308,7 @@ cdef bool IntListComparison(Py_ssize_t[:] l1, Py_ssize_t[:] l2, size_t length) n
 @cython.wraparound(False)
 @cython.boundscheck(False)
 cdef vector[vector[vector[double]]] cgetQuads(int[:,:] faces_mv,
-               size_t[:] fs, double[:,:] verts_mv, int[:] verts_label_map_mv):
+               size_t[:] fs, double[:,:] verts_mv, int[:] verts_label_map_mv) nogil:
     cdef: 
         size_t idx, i, j
         vector[vector[vector[double]]] vquads
@@ -293,20 +330,18 @@ cdef vector[vector[vector[double]]] cgetQuads(int[:,:] faces_mv,
 
 @cython.wraparound(False)
 @cython.boundscheck(False)
-cdef SymmetricMatrix intra_face_distance_matrix(double[:,:] quad_face):
+cdef void intra_face_distance_matrix(double[:,:] quad_face, SymmetricMatrix m) nogil:
     cdef :
-        SymmetricMatrix m = SymmetricMatrix(size=4)
-        Py_ssize_t i, j 
+        size_t i, j 
         double[:] pt1, pt2
     for i in range(4):
         for j in range(i,4):
             if i == j :
-                m[i,j] = 0.0
+                m.set_item_nogil(i, j, 0.0)
             else : 
                 pt1 = quad_face[i,:]
                 pt2 = quad_face[j,:]
-                m[i,j] = distance3D(pt1, pt2)
-    return m
+                m.set_item_nogil(i, j, distance3D(pt1, pt2))
 
 #############################################################################
 
@@ -356,7 +391,7 @@ cdef vector[double] cross_product(double[:] vect1,double[:] vect2):
 
 @cython.wraparound(False)
 @cython.boundscheck(False)
-cdef vector[double] cross_product_vect(vector[double] vect1, vector[double] vect2):
+cdef vector[double] cross_product_vect(vector[double] vect1, vector[double] vect2) nogil:
     """Cross product between 2 3D vectors (x1, y1, z1), (x2, y2, z2)
     """
     cdef double x_cross, y_cross, z_cross 
@@ -375,7 +410,7 @@ cdef vector[double] cross_product_vect(vector[double] vect1, vector[double] vect
 
 @cython.wraparound(False)
 @cython.boundscheck(False)
-cdef vector[double] quad_centroid(double[:,:] quad):
+cdef vector[double] quad_centroid(double[:,:] quad) nogil:
     """Centroid of quadrilateral element of shape (4,3)
     """
     cdef double x_sum, y_sum, z_sum 
@@ -393,7 +428,7 @@ cdef vector[double] quad_centroid(double[:,:] quad):
 
 @cython.wraparound(False)
 @cython.boundscheck(False)
-cdef vector[vector[double]] quadCenterEdgeVectors(double[:,:] quad, vector[double] centroid):
+cdef vector[vector[double]] quadCenterEdgeVectors(double[:,:] quad, vector[double] centroid) nogil:
     """list of vectors going from the centroid of the element to the edges.
     if the centroid is (xc,yc,zc) and the quad is
         ((x1, y1, z1),
@@ -424,7 +459,7 @@ cdef vector[vector[double]] quadCenterEdgeVectors(double[:,:] quad, vector[doubl
 
 @cython.wraparound(False)
 @cython.boundscheck(False)
-cdef vector[double] quadNormal(double[:,:] quad):
+cdef vector[double] quadNormal(double[:,:] quad) nogil:
     """For each successive centroid-edge vector calculated above, we calculate the associated
     normal using the cross product. Then we avarage the four normals to get the approximated
     closest normal.
@@ -455,7 +490,8 @@ cdef vector[double] quadNormal(double[:,:] quad):
 
 @cython.wraparound(False)
 @cython.boundscheck(False)
-cdef vector[double] vector2unit(vector[double] vect):
+@cython.cdivision(True)
+cdef vector[double] vector2unit(vector[double] vect) nogil:
     """norm of n dimensional vector
     """
     cdef vector[double] unitVector
@@ -471,7 +507,7 @@ cdef vector[double] vector2unit(vector[double] vect):
 
 @cython.wraparound(False)
 @cython.boundscheck(False)
-cdef double norm_vector(vector[double] vect):
+cdef double norm_vector(vector[double] vect) nogil:
     """norm of n dimensional vector
     """
     cdef double squared_sum 
@@ -486,7 +522,7 @@ cdef double norm_vector(vector[double] vect):
 
 @cython.wraparound(False)
 @cython.boundscheck(False)
-cdef vector[double] sum_vectors(vector[double] vect1, vector[double] vect2):
+cdef vector[double] sum_vectors(vector[double] vect1, vector[double] vect2) nogil:
     """returns the vector sum of two vectors
     """
     cdef vector[double] sum_vects 
@@ -534,6 +570,13 @@ cdef class SymmetricMatrix:
 
     def argmax(self):
         return self.cargmax()
+
+    @cython.wraparound(False)
+    @cython.boundscheck(False)
+    cdef size_t set_item_nogil(self, size_t row, size_t column, double value) nogil:
+        cdef size_t index
+        index = self._get_index(row, column)
+        self._data[index] = value
 
     @cython.wraparound(False)
     @cython.boundscheck(False)
